ca9e7972c656bde0184ff0e3bb46367328681f55
Matheus Herman <69770331+matheusherman@users.noreply.github.com>
2022-06-30 10:58:07 -0300

    Update ArvoreBin.java

--- Arvore/src/ArvoreBin.java
+++ Arvore/src/ArvoreBin.java
@@ -1,32 +1,48 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class ArvoreBin {
-    private No raiz;
+
+    private List<No> arvore;
 
     public ArvoreBin() { //construtor
-        this.raiz = null;
+        arvore = new LinkedList<>();
     }
 
     public No ObtemRaiz() {
-        return this.raiz;
+        return arvore.get(0);
     }
 
     public void Adicionar(int novo) {
         No n = new No(novo);
-        this.raiz = this.Inserir(n, this.raiz);
+        arvore.add(n);
     }
 
-    private No Inserir(No novo, No inicio) {
-
-        if (inicio != null) {
-            if (inicio.p_Esq == null) {
-                inicio.p_Esq = this.Inserir(novo, inicio.p_Esq);
-
-            } else if (inicio.p_Dir == null) {
-                inicio.p_Dir = this.Inserir(novo, inicio.p_Dir);
-            } else
-                inicio.p_Esq = this.Inserir(novo, inicio.p_Esq);
-                return inicio;
-        } else
-            return novo;
+    public Integer Busca(int busca) {
+        for (int i = 0; i < arvore.size(); i++) {
+            if (busca == arvore.get(i).valor)
+                return busca;
+        }
+        return null;
     }
 
+    public String imprime() {
+        StringBuilder sb = new StringBuilder();
+        int nivel = 0;
+        int lastIndex = 0;
+        while (lastIndex < arvore.size()) {
+            int maxIndex = lastIndex + (int) Math.pow(2, nivel) - 1;
+            if (maxIndex >= arvore.size()) {
+                maxIndex = arvore.size() - 1;
+            }
+            sb.append("Nível " + nivel + "\n");
+            for (; lastIndex <= maxIndex; lastIndex++) {
+                sb.append(arvore.get(lastIndex).valor + "\t");
+            }
+            sb.append("\n");
+            nivel++;
+        }
+        return sb.toString();
+    }
 }
+
